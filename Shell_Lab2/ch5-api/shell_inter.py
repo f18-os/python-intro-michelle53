fb714f0aa3879063778f6fcda41cb8650027c132
michelle53 <michelleesmeraldapatino@gmail.com>
2018-09-10 19:14:59 -0600

    commented and cleaned code

--- Shell_Lab2/ch5-api/shell_inter.py
+++ Shell_Lab2/ch5-api/shell_inter.py
@@ -10,35 +10,35 @@ if rc < 0:
 
 elif rc == 0:                   # child
    
-    file_out = ''
-    file_in = ''
-    r_direct = False
-    l_direct = False
-    args = input('>> ') # get command
-    if len(args) == 0 :
+    file_out = '' # file to write to
+    
+    r_direct = False # check if output redirection
+    l_direct = False # check if input redirection
+    args = input('>> ') # get command 
+    if len(args) == 0 : #if no argument exit
         sys.exit(1)
-    if '>' in args:
+    if '>' in args: # right redirection
         r_direct = True
-    elif '<' in args:
+    elif '<' in args: # left redirection
         l_direct = True
-    args = re.split('[><]', args)
-    if len(args) == 2:
-        if r_direct == True:
+    args = re.split('[><]', args) # split
+    if len(args) == 2: # if there is redirection
+        if r_direct == True: # output redirection
             file_out = re.split(' ', args[1])
             file_out = file_out[1]
             args = re.split(' ', args[0])
             args = args[:-1]
             os.close(1)
-        elif l_direct == True:
+        elif l_direct == True: # input redirection
             file_in = re.split(' ', args[1])
             file_in = file_in[1]
             args = re.split(' ', args[0])
             args = args[:-1]
             args.append(file_in)
-    else:
+    else: # no redirection
         args = re.split(' ', args[0])
-        # os.close(1)
-    if file_out != '':
+      
+    if file_out != '': # set file to be written to as output
         sys.stdout = open(file_out, 'w' )
         fd = sys.stdout.fileno()
         os.set_inheritable(fd, True)              
